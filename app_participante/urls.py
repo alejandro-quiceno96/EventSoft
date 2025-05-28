4a1789ac161f26fdadcec71ba873bf8e706dbaa5
alejandro-quiceno96 <alejandroquiceno2006@gmail.com>
2025-05-28 13:59:12 -0500

    Avances en el cambio de framwork

--- app_participante/urls.py
+++ app_participante/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('ver_info_participante/', views.info_participantes_eventos, name='ver_info_participante'),
     path("event/<int:evento_id>/participante/<int:participante_id>", views.evento_detalle_participante, name= "detalle_evento"),
     path('generar_pdf_criterios/<int:evento_id>', views.generar_pdf_criterios, name= "generar_pdf_criterios"),
-    
+    path("obtener_datos_participante/<int:participante_id>/evento/<int:evento_id>", views.obtener_datos_participante, name="obtener_datos_participante"),
+    path('modificar_inscripcion/<int:evento_id>/partticipante/<int:participante_id>', views.modificar_inscripcion, name='modificar_inscripcion'),
+    path('cancelar_inscripcion/<int:evento_id>/participante/<int:participante_id>', views.cancelar_inscripcion, name = "cancelar_inscripcion" )
 
 ]
 
