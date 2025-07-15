de73b1ce007f8c33e3fa5a268a092c95bd6e8eb9
alejandro-quiceno96 <alejandroquiceno2006@gmail.com>
2025-07-15 18:00:10 -0500

    Envio de certificados a los asistentes y evaluadores

--- app_administrador/urls.py
+++ app_administrador/urls.py
@@ -38,6 +38,8 @@ urlpatterns=[
     #correos
     path('enviar_correo/<int:evento_id>', views.enviar_correo, name='enviar_correo'),
     path('enviar_certificado_participantes/<int:evento_id>/', views.enviar_certificado_participantes, name='enviar_certificado_participantes'),
+    path('enviar_certificado_asistentes/<int:evento_id>/', views.enviar_certificado_asistentes, name='enviar_certificado_asistentes'),
+    path('enviar_certificado_evaluadores/<int:evento_id>/', views.enviar_certificado_evaluadores, name='enviar_certificado_evaluadores'),
 
     #memorias
     path('guardar-memorias/', views.guardar_memorias, name='guardar_memorias'),
