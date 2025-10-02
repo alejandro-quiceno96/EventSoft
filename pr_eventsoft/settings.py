d852968133370a4846eae201f8d60abb0243dfd8
alejandro-quiceno96 <alejandroquiceno2006@gmail.com>
2025-10-02 11:52:54 -0500

    cambios en el settings

--- pr_eventsoft/settings.py
+++ pr_eventsoft/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ["eventsoft.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 
