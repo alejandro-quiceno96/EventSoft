10f90f5428d3e3f73d8a37bc39ed978c17c5c2e1
alejandro-quiceno96 <alejandroquiceno2006@gmail.com>
2025-10-02 11:28:40 -0500

    cambiando setting de la db en render

--- pr_eventsoft/settings.py
+++ pr_eventsoft/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://eventsoft.onrender.com"]
 
 
 # Application definition
