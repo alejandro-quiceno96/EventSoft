75916e6e11fc8330096b74a580ae21b656e33746
alejandro-quiceno96 <alejandroquiceno2006@gmail.com>
2025-10-27 21:50:25 -0500

    Avances en la pruebas automatizadas

--- pr_eventsoft/settings.py
+++ pr_eventsoft/settings.py
@@ -81,8 +81,15 @@ WSGI_APPLICATION = 'pr_eventsoft.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'eventsoft'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
