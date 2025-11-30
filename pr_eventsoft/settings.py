45ecf32079948027102ff1a9e122da42d79d90db
sebas-q-design <sebasxd782@gmail.com>
2025-11-30 12:57:11 -0500

    despliegue

--- pr_eventsoft/settings.py
+++ pr_eventsoft/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'pr_eventsoft.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'eventsoft'),
-        'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST',),
         'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
-            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
-}
+}  
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
