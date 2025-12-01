d3f4fbdf22114a8273abcb5e82893e5448b72f89
sebas-q-design <sebasxd782@gmail.com>
2025-12-01 07:34:17 -0500

    despliegue

--- pr_eventsoft/settings.py
+++ pr_eventsoft/settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import sys
+import dj_database_url
 
 load_dotenv()  # cargar variables desde .env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,15 +81,13 @@ WSGI_APPLICATION = 'pr_eventsoft.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
