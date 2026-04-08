3070b1771108c95775c307401f9895dc6d3dffa0
alejandro-quiceno96 <alejandroquiceno2006@gmail.com>
2026-04-07 20:19:49 -0500

    Fix email config dynamic port

--- pr_eventsoft/settings.py
+++ pr_eventsoft/settings.py
@@ -146,11 +146,17 @@ STATICFILES_DIRS = [
 # Configuración de correo utilizando decouple para mayor robustez
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='smtp-relay.brevo.com')
+# Lógica dinámica: Si el puerto es 465 usar SSL, si es 587 usar TLS
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
+
+if EMAIL_PORT == 465:
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
+else:
+    EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+    EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
 
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='eventsoft3@gmail.com')
 
